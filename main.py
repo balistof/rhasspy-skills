1b89ddf4a87ad5a1ec8ee1c14f076ad8a90d9603
Christoph Fröschl <christoph.froeschl@gmail.com>
2021-01-28 08:08:32 +0100

    feat: base setup

---
+++ main.py
@@ -0,0 +1,27 @@
+"""Example app to react to an intent to tell you the time."""
+import logging
+from datetime import datetime
+
+from rhasspyhermes.nlu import NluIntent, NluIntentNotRecognized
+
+from rhasspyhermes_app import EndSession, HermesApp
+
+_LOGGER = logging.getLogger("BaseApp")
+
+app = HermesApp("BaseApp")
+
+@app.on_intent("GetTime")
+async def get_time(intent: NluIntent):
+    """Tell the time."""
+    now = datetime.now().strftime("%H Uhr %M")
+    return EndSession(f"Es ist {now}")
+
+@app.on_intent("GetWeather")
+async def get_time(intent: NluIntent):
+    return EndSession(f"das muß mir erst jemand lernen")
+
+@app.on_intent_not_recognized
+async def not_understood(intent_not_recognized: NluIntentNotRecognized):
+    return EndSession(f"{intent_not_recognized.input} kenne ich nicht")
+
+app.run()
