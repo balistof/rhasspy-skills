8cbc58608eb7099d63e88e7644e0de67f2dc5ed3
Christoph Fröschl <christoph@christophfroeschl.com>
2021-02-19 19:14:06 +0100

    feat: improve news handling

--- news/__init__.py
+++ news/__init__.py
@@ -1,24 +1,45 @@
 import dialog
 import feedparser
+import random
 from rhasspyhermes.nlu import NluIntent
+from io import StringIO
+from html.parser import HTMLParser
 
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.text = StringIO()
+    def handle_data(self, d):
+        self.text.write(d)
+    def get_data(self):
+        return self.text.getvalue()
+
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
 
 @dialog.app.on_intent("News")
 async def news(intent: NluIntent):
     print("News")
     try:
-        # news_feed = feedparser.parse("https://rss.orf.at/news.xml")
-        news_feed = feedparser.parse("https://rss.orf.at/oe3.xml")
-        news_text = "die schlagzeilen lauten. "
-        max_entries = 3
-        i = 0
+        #news_feed = feedparser.parse("https://rss.orf.at/oe3.xml")
+        news_feed = feedparser.parse("https://www.derstandard.at/rss")
+        news = []
         for entry in news_feed["entries"]:
-            if i >= max_entries:
-                break
             if "/sendungen/" in entry["link"]:
                 continue
-            news_text += entry["title"] + ". " + entry["description"] + ". . "
-            i += 1
+            news.append(entry)
+
+        news_text = "die schlagzeilen lauten. "
+        for entry in random.sample(news, 3):
+            news_text += entry["title"] + ". " + strip_tags(entry["description"]) + ". . "
+
         return dialog.responseOK(news_text)
     except Exception as ex:
         return dialog.responseError(ex)
